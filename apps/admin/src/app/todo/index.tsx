bfa882f23860d651ac1929ad1bb5d09122587646
sarathirangasamy <sarathirangasamy@gmail.com>
2023-12-23 07:58:55 +0530

    css added

--- apps/admin/src/app/todo/index.tsx
+++ apps/admin/src/app/todo/index.tsx
@@ -1,6 +1,39 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+interface PropType {
+  todoDetailsInfo: string[];
+  page: number;
+  setPage: React.Dispatch<React.SetStateAction<number>>;
+}
 
-export const TodoList: React.FC = () => {
+export const TodoList: React.FC<PropType> = ({
+  todoDetailsInfo,
+  page,
+  setPage,
+}) => {
   return (
-      <p>To do List</p>
+    <>
+      <div className="list-container">
+        {todoDetailsInfo?.map((todo: any, index) => (
+          <div className="to-do-item">{todo?.name.toUpperCase()}</div>
+        ))}
+      </div>
+
+      <div>
+        <button
+          className="pagination-btn"
+          onClick={() => setPage((prevPage) => Math.max(prevPage - 1, 1))}
+        >
+          Previous Page
+        </button>
+        <span className="page-text"> {page}</span>
+        <button
+          className="pagination-btn"
+          disabled={todoDetailsInfo?.length < 5 ? true : false}
+          onClick={() => setPage((prevPage) => prevPage + 1)}
+        >
+          Next Page
+        </button>
+      </div>
+    </>
   );
 };
--- apps/admin/src/app/todo/todo-form/index.tsx
+++ apps/admin/src/app/todo/todo-form/index.tsx
@@ -1,16 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { config, environment } from 'apps/admin/src/environments/environments';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-
 import { TodoList } from '..';
+import { config, environment } from 'apps/admin/src/environments/environments';
+import './style.less';
 
 export const TodoForm: React.FC = () => {
   const [todoDetailsInfo, setTodoDetailsInfo] = useState<string[]>([]);
   const [todoValue, setTodoValue] = useState<string>('');
   const [page, setPage] = useState<number>(1);
   const [skip, setSkip] = useState<number>(0);
-  const pageSize = 10;
+  const pageSize = 5;
 
   useEffect(() => {
     const newSkip = (page - 1) * pageSize;
@@ -47,62 +47,40 @@ export const TodoForm: React.FC = () => {
       .catch((error) => {
         console.error('Error:', error);
       });
-
-    // await axios
-    //   .post(`${environment?.port}/create`, { name: todoValue })
-    //   .then((data) => {
-    //     setTodoValue('');
-    //   });
   };
 
-  const removeTodo = (index: number) => {
-    const updatedTasks = [...todoDetailsInfo];
-    updatedTasks.splice(index, 1);
-    setTodoDetailsInfo(updatedTasks);
-  };
+  // const removeTodo = (index: number) => {
+  //   const updatedTasks = [...todoDetailsInfo];
+  //   updatedTasks.splice(index, 1);
+  //   setTodoDetailsInfo(updatedTasks);
+  // };
 
   return (
-    <>
-      <div className="todo-container">
-        <h1>To-Do List</h1>
-        <div className="input-container">
-          <input
-            type="text"
-            className="custom-input"
-            placeholder="Add a new todo"
-            value={todoValue}
-            onChange={(e) => setTodoValue(e.target.value)}
-          />
-          <button disabled={todoValue ? false : true} onClick={addTodo}>
-            Add
-          </button>
-        </div>
-
-        <ul className="task-list">
-          {todoDetailsInfo?.map((todo: any, index) => (
-            <li key={index}>
-              {todo?.name}
-              <button onClick={() => removeTodo(index)}>Remove</button>
-            </li>
-          ))}
-        </ul>
-
-        {/* Pagination controls */}
+    <div className="todo-container">
+      <h1 className="page-text">To-Do List</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          className="custom-input"
+          placeholder="Add a new todo"
+          value={todoValue}
+          onChange={(e) => setTodoValue(e.target.value)}
+        />
         <button
-          onClick={() => setPage((prevPage) => Math.max(prevPage - 1, 1))}
+          className="custom-add-btn"
+          disabled={todoValue ? false : true}
+          onClick={addTodo}
         >
-          Previous Page
-        </button>
-        <span>Page {page}</span>
-        <button
-          disabled={todoDetailsInfo?.length < 10 ? true : false}
-          onClick={() => setPage((prevPage) => prevPage + 1)}
-        >
-          Next Page
+          ADD
         </button>
       </div>
+      <hr />
 
-      <TodoList />
-    </>
+      <TodoList
+        todoDetailsInfo={todoDetailsInfo}
+        page={page}
+        setPage={setPage}
+      />
+    </div>
   );
 };
